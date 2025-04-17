a224c38c4f4f13f64d783c4825307c9543aa395c
rdubsky17 <117395112+rdubsky17@users.noreply.github.com>
2025-04-17 15:27:08 -0500

    Update StatReader.py
    Modified File Paths for Exe

--- StatReader.py
+++ StatReader.py
@@ -1,9 +1,20 @@
 import pandas as pd
 from datetime import datetime, timedelta
 import glob
+import os, sys
+
+def get_runtime_path(filename_or_folder):
+    if getattr(sys, 'frozen', False):
+        return os.path.join(os.path.dirname(sys.executable), filename_or_folder)
+    return os.path.abspath(filename_or_folder)
+
+GAMESTATS_PATH = "GameStats"
+
+
+
 pd.set_option('display.max_colwidth', None)
 def getStats(_playerName):
-    file_pattern = "RLTracker/GameStats/*.csv"
+    file_pattern = os.path.join(get_runtime_path(GAMESTATS_PATH), "*.csv")
     csv_files = glob.glob(file_pattern)
     print("CSV files found:", csv_files)
     dataframeList = []
@@ -21,13 +32,12 @@ def getStats(_playerName):
         dataframeList.append(df)
 
     gamestats_df = pd.concat(dataframeList)
-    spotify_df = pd.read_csv("RLTracker/spotify_log.csv")
     goku_stats = gamestats_df[gamestats_df['PlayerName'] == _playerName]
     gamestats_df = goku_stats
-    gamestats_df.to_csv("RLTracker/gamestats.csv")
+    #gamestats_df.to_csv("gamestats.csv")
 
 
-
+    spotify_df = pd.read_csv(get_runtime_path("spotify_log.csv"))
     spotify_df['Timestamp'] = pd.to_datetime(spotify_df['Timestamp'])
     gamestats_df['Timestamp'] = gamestats_df['Timestamp'].apply(lambda x: datetime.strptime(x, '%Y-%m-%d_%H-%M-%S'))
     
@@ -47,7 +57,7 @@ def getStats(_playerName):
     spotify_df = spotify_df.dropna(subset=['GameEnd']) # Deletes all non-matching songs
 
 
-    spotify_df.to_csv("openme.csv")
+    #spotify_df.to_csv("completelog.csv", index=False) # All songs that played during rl
 
     gamestats_df['Timestamp'] = pd.to_datetime(gamestats_df['Timestamp'], format='%Y-%m-%d %H:%M:%S')
     spotify_df['GameEnd'] = pd.to_datetime(spotify_df['GameEnd'], format='%Y-%m-%d %H:%M:%S')
@@ -64,35 +74,35 @@ def getStats(_playerName):
     )
 
 
-    combined_df.to_csv('combined.csv', index=False)
+    #combined_df.to_csv('combined.csv', index=False)
 
     columns_to_keep = ['GameEnd', 'PlayerName', 'Score', 'Goals', 'Assists', 'Saves', 'Shots', 'Demolishes', 'BoostUsage', 'PossessionTime', 'Artist', 'Track', 'Wins']
     combinedandrefined_df = combined_df[columns_to_keep].copy()
-    combinedandrefined_df.to_csv('combinedandrefined.csv', index=False)
+    #combinedandrefined_df.to_csv('combinedandrefined.csv', index=False)
 
 
     combinedandrefined_df['Count'] = 1
 
     combinedandrefined_df['PossessionTime'] = pd.to_timedelta("0:" + combinedandrefined_df['PossessionTime'])
     numeric_cols = ['Score', 'Goals', 'Assists', 'Saves', 'Shots', 'Demolishes', 'BoostUsage', 'PossessionTime', 'Wins', 'Count']
-    superstats = combinedandrefined_df.groupby(['Artist', 'Track'], as_index=False)[numeric_cols].sum()
+    # superstats = combinedandrefined_df.groupby(['Artist', 'Track'], as_index=False)[numeric_cols].sum()
     
-
-    superstats['Goal_Average'] = superstats['Goals'] / superstats['Count']
-    superstats['Win_Average'] = superstats['Wins'] / superstats['Count']
-    superstats['Save_Average'] = superstats['Saves'] / superstats['Count']
-    superstats['Shot_Average'] = superstats['Shots'] / superstats['Count']
-    superstats['Assist_Average'] = superstats['Assists'] / superstats['Count']
-    superstats['Demoloshes_Average'] = superstats['Demolishes'] / superstats['Count']
-    superstats['BoostUsage_Average'] = superstats['BoostUsage'] / superstats['Count']
-    superstats['PossessionTime_Average'] = superstats['PossessionTime'] / superstats['Count']
-    # sorted_df = superstats.sort_values(by='Goal_Average', ascending= False)
-    # sorted_df.to_csv('superstats.csv', index=False)
-    return superstats
+    # superstats['Score_Average'] = superstats['Score'] / superstats['Count']
+    # superstats['Goals_Average'] = superstats['Goals'] / superstats['Count']
+    # superstats['Wins_Average'] = superstats['Wins'] / superstats['Count']
+    # superstats['Saves_Average'] = superstats['Saves'] / superstats['Count']
+    # superstats['Shots_Average'] = superstats['Shots'] / superstats['Count']
+    # superstats['Assists_Average'] = superstats['Assists'] / superstats['Count']
+    # superstats['Demoloshes_Average'] = superstats['Demolishes'] / superstats['Count']
+    # superstats['BoostUsage_Average'] = superstats['BoostUsage'] / superstats['Count']
+    # superstats['PossessionTime_Average'] = superstats['PossessionTime'] / superstats['Count']
+    # # sorted_df = superstats.sort_values(by='Goal_Average', ascending= False)
+    # superstats.to_csv("AllSongsAndStats.csv", index=False)
+    return combinedandrefined_df
 
 if __name__ == '__main__':
     df = getStats("Goku SSJGSS")
     print(getStats("Goku SSJGSS"))
-    sorted_df = df.sort_values(by=['Goal_Average', 'Count'], ascending= [False, False])
-    sorted_df.to_csv('superstats.csv', index=False)
+    #sorted_df = df.sort_values(by=['Goal_Average', 'Count'], ascending= [False, False])
+    #sorted_df.to_csv('sortedstats.csv', index=False)
     
